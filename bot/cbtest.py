55cf7e3bfb2770ef636c9d830fc17543a924ea4a
Sonyy172 <phuc.nh94@gmail.com>
2017-12-05 14:33:35 +0700

    a

--- bot/cbtest.py
+++ bot/cbtest.py
@@ -1,5 +1,5 @@
 
-def postback_handler_cbtest(event):
+def cbtest_postback_handler(event):
     print('POSTBACK HANDLER CBTEST')
     sender_id = event.sender_id
     postback = event.postback_payload
@@ -9,23 +9,20 @@ def postback_handler_cbtest(event):
 
     if postback in postback_list:
         postback_list[postback](sender_id)
-    return 'cbtest postback ok'
 
 
-# def message_handler_cbtest(event):
-#     print('MESSAGE HANDLER CBTEST')
-#     sender_id = event.sender_id
-#     message = event.message_text
-#     quickreply = event.quick_reply_payload
+def cbtest_message_handler(event):
+    print('MESSAGE HANDLER CBTEST')
+    sender_id = event.sender_id
+    message = event.message_text
+    quickreply = event.quick_reply_payload
 
-#     if message is not None:
-#         message = message.lower()
-#     else:
-#         pass
+    if message is not None:
+        message = message.lower()
+    else:
+        pass
 
-#     handle_mess(sender_id, message)
-
-#     return 'cbtest message ok'
+    # handle_mess(sender_id, message)
 
 
 def upload_image(sender_id):
